1b54a2bb3b7d255a38d2e9868001f360239acafd
devkilyungi <victorkilyungi@gmail.com>
2023-05-11 11:03:20 +0300

    minor modifications

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const featureNavbarItem = document.querySelector("#first-dropdown");
 const featureDropdown = document.querySelector("#first-dropdown-content");
+const featureArrow = document.querySelector("#first-arrow")
 const companyNavbarItem = document.querySelector("#second-dropdown");
 const companyDropdown = document.querySelector("#second-dropdown-content");
+const companyArrow = document.querySelector("#second-arrow")
 
 let isHoveringOverFeature = false;
 let isHoveringOverCompany = false;
@@ -9,14 +11,16 @@ let isHoveringOverCompany = false;
 featureNavbarItem.addEventListener('mouseenter', () => {
   isHoveringOverFeature = true;
   featureDropdown.classList.add('show-dropdown')
+  featureArrow.setAttribute('src','./images/icon-arrow-up.svg')
   featureDropdown.style.top = '40px'
   let interval = setInterval(() => {
     if (isHoveringOverFeature) { return; }
     else {
       clearInterval(interval);
       featureDropdown.classList.remove('show-dropdown')
+      featureArrow.setAttribute('src','./images/icon-arrow-down.svg')
     }
-  }, 250)
+  }, 200)
 })
 
 featureNavbarItem.addEventListener('mouseleave', ()=>{
@@ -26,13 +30,15 @@ featureNavbarItem.addEventListener('mouseleave', ()=>{
 companyNavbarItem.addEventListener('mouseenter', () => {
   isHoveringOverCompany = true;
   companyDropdown.classList.add('show-dropdown')
+  companyArrow.setAttribute('src','./images/icon-arrow-up.svg')
   let interval = setInterval(() => {
     if (isHoveringOverCompany) { return; }
     else {
       clearInterval(interval);
       companyDropdown.classList.remove('show-dropdown')
+      companyArrow.setAttribute('src','./images/icon-arrow-down.svg')
     }
-  }, 250)
+  }, 200)
 })
 
 companyNavbarItem.addEventListener('mouseleave', ()=>{
